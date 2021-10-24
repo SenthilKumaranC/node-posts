7e585769b92f057cb633fdfe0fb4b3faa78e47b1
unknown <senthil.kumaran@gridlogic.in>
2021-10-25 00:24:42 +0530

    'Changes'

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://mongodb:mongodb@cluster0.k4ntk.mongodb.net/firebook?retryWrites=true&w=majority'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://mongodb:mongodb@cluster0.k4ntk.mongodb.net/firebook?retryWrites=true&w=majority'), UsersModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
